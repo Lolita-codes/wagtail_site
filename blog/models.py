fabf08345f17e6daece4729217a94329f64a9ff7
Lolita <97053333+Lolita-codes@users.noreply.github.com>
2023-01-29 16:15:19 +0100

    Add Routable pages

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.shortcuts import render
 from wagtail.admin.panels import FieldPanel
+from wagtail.contrib.routable_page.models import route, RoutablePageMixin
 from wagtail.fields import StreamField
 
 from wagtail.models import Page
@@ -7,7 +9,7 @@ from wagtail.models import Page
 from streams import blocks
 
 
-class BlogListingPage(Page):
+class BlogListingPage(RoutablePageMixin, Page):
     """Listing page lists all the Blog Detail Pages."""
 
     template = "blog/blog_listing_page.html"
@@ -28,6 +30,12 @@ class BlogListingPage(Page):
         context = super().get_context(request, *args, **kwargs)
         context['posts'] = BlogDetailPage.objects.live().public()
         return context
+
+    @route(r'^latest/?$', name="latest_posts")
+    def latest_blog_posts(self, request, *args, **kwargs):
+        context = self.get_context(request, *args, **kwargs)
+        context["latest_posts"] = context["posts"][:1]
+        return render(request, "blog/latest_posts.html", context)
 
 
 class BlogDetailPage(Page):
--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.shortcuts import render
 from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, InlinePanel, MultiFieldPanel
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.fields import RichTextField, StreamField
 from wagtail.images.edit_handlers import ImageChooserPanel
 
@@ -26,7 +28,7 @@ class HomePageCarouselImages(Orderable):
     ]
 
 
-class HomePage(Page):
+class HomePage(RoutablePageMixin, Page):
     """Home page model"""
     templates = "home/home_page.html"
     banner_title = models.CharField(max_length=100, blank=False, null=True)
@@ -70,3 +72,9 @@ class HomePage(Page):
     class Meta:
         verbose_name = 'Home Page'
         verbose_name_plural = 'Home Pages'
+
+    @route(r'^subscribe/$')
+    def the_subscribe_page(self, request, *args, **kwargs):
+        context = self.get_context(request, *args, **kwargs)
+        context['a_special_test'] = 'Hello World 123123'
+        return render(request, "home/subscribe.html", context)
