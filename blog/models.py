bff4e56edc2710016af9aa56c3135196b60579b4
Lolita <97053333+Lolita-codes@users.noreply.github.com>
2023-01-29 21:24:39 +0100

    Register Snippets with Django model using FieldPanel and Checkboxes

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,98 @@
 from django.db import models
+from django import forms
 from django.shortcuts import render
-from wagtail.admin.panels import FieldPanel
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.contrib.routable_page.models import route, RoutablePageMixin
 from wagtail.fields import StreamField
 
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
+from wagtail.snippets.models import register_snippet
 
 from streams import blocks
+
+
+class BlogAuthorsOrderable(Orderable):
+    """This allows us to select one or more blog authors from Snippets."""
+
+    page = ParentalKey("blog.BlogDetailPage", related_name="blog_authors")
+    author = models.ForeignKey(
+        "blog.BlogAuthor",
+        on_delete=models.CASCADE,
+    )
+
+    panels = [
+        FieldPanel("author"),
+    ]
+
+
+class BlogAuthor(models.Model):
+    """Blog author for snippets."""
+
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True, null=True)
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="+",
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("name"),
+                FieldPanel("image"),
+            ],
+            heading="Name and Image",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("website"),
+            ],
+            heading="Links"
+        )
+    ]
+
+    def __str__(self):
+        """String repr of this class."""
+        return self.name
+
+    class Meta:  # noqa
+        verbose_name = "Blog Author"
+        verbose_name_plural = "Blog Authors"
+
+
+register_snippet(BlogAuthor)
+
+
+class BlogCategory(models.Model):
+    """Blog catgory for a snippet."""
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        verbose_name="slug",
+        allow_unicode=True,
+        max_length=255,
+        help_text='A slug to identify posts by this category',
+    )
+
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("slug"),
+    ]
+
+    class Meta:
+        verbose_name = "Blog Category"
+        verbose_name_plural = "Blog Categories"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+register_snippet(BlogCategory)
 
 
 class BlogListingPage(RoutablePageMixin, Page):
@@ -29,6 +115,7 @@ class BlogListingPage(RoutablePageMixin, Page):
         """Adding custom stuff to our context."""
         context = super().get_context(request, *args, **kwargs)
         context['posts'] = BlogDetailPage.objects.live().public()
+        context["categories"] = BlogCategory.objects.all()
         return context
 
     @route(r'^latest/?$', name="latest_posts")
@@ -81,8 +168,23 @@ class BlogDetailPage(Page):
         use_json_field=True,
     )
 
+    categories = ParentalManyToManyField("blog.BlogCategory", blank=True)
+
     content_panels = Page.content_panels + [
         FieldPanel("custom_title"),
         FieldPanel("blog_image"),
+        FieldPanel("content"),
+        MultiFieldPanel(
+            [
+                InlinePanel("blog_authors", label="Author", min_num=1, max_num=4)
+            ],
+            heading="Author(s)"
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("categories", widget=forms.CheckboxSelectMultiple)
+            ],
+            heading="Categories"
+        ),
         FieldPanel("content"),
     ]
