24dcfdb5cf7963ae8c8c1c6a6c657af470c57205
Lolita <97053333+Lolita-codes@users.noreply.github.com>
2023-01-30 12:00:58 +0100

    Add child pages

--- blog/models.py
+++ blog/models.py
@@ -147,7 +147,7 @@ class BlogDetailPage(Page):
         null=False,
         help_text='Overwrites the default title',
     )
-    blog_image = models.ForeignKey(
+    banner_image = models.ForeignKey(
         "wagtailimages.Image",
         blank=False,
         null=True,
@@ -172,7 +172,7 @@ class BlogDetailPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel("custom_title"),
-        FieldPanel("blog_image"),
+        FieldPanel("banner_image"),
         FieldPanel("content"),
         MultiFieldPanel(
             [
@@ -186,5 +186,73 @@ class BlogDetailPage(Page):
             ],
             heading="Categories"
         ),
+        FieldPanel("content"),
+    ]
+
+
+# First subclassed blog post page
+class ArticleBlogPage(BlogDetailPage):
+    """A subclassed blog post page for articles."""
+
+    template = "blog/article_blog_page.html"
+
+    subtitle = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+    intro_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        help_text='Best size for this image will be 1400x400'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+        FieldPanel("subtitle"),
+        FieldPanel("banner_image"),
+        FieldPanel("intro_image"),
+        MultiFieldPanel(
+            [
+                InlinePanel("blog_authors", label="Author", min_num=1, max_num=4)
+            ],
+            heading="Author(s)"
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("categories", widget=forms.CheckboxSelectMultiple)
+            ],
+            heading="Categories"
+        ),
+        FieldPanel("content"),
+    ]
+
+
+# Second subclassed page
+class VideoBlogPage(BlogDetailPage):
+    """A video subclassed page."""
+
+    template = "blog/video_blog_page.html"
+
+    youtube_video_id = models.CharField(max_length=30)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+        FieldPanel("banner_image"),
+        MultiFieldPanel(
+            [
+                InlinePanel("blog_authors", label="Author", min_num=1, max_num=4)
+            ],
+            heading="Author(s)"
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("categories", widget=forms.CheckboxSelectMultiple)
+            ],
+            heading="Categories"
+        ),
+        FieldPanel("youtube_video_id"),
         FieldPanel("content"),
     ]
